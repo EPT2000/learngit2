9aa66a3a77cf2cdace5038e187898559ac41a54e
Mayank Tripathi <mr.mayanktripathi2001@gmail.com>
2025-01-28 09:09:34 +0530

    learning git additio
    along with readme

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log("Hello World");
+console.log(3+6)
+console.log(3*6)
+console.log(3/6)
+console.log(3%6)
+console.log(3**6)
+console.log(3**6)
+
+let x = 10;
+let y = 20;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y);
+
+let a = 10;
+let b = 20;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+console.log(a**b);
